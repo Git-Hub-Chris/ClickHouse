7501462383e2d0ee5fe2b253220c4876591ed86d
NamHoaiNguyen <nguyen.hoai.nam.3912@gmail.com>
2025-01-17 14:53:41 +0700

    Fix test case

--- tests/queries/0_stateless/03312_issue_74299.sql
+++ tests/queries/0_stateless/03312_issue_74299.sql
@@ -2,10 +2,6 @@ DROP TABLE if exists t0;
 
 CREATE TABLE t0 (c0 Int) ENGINE = Memory;
 
-INSERT INTO t0 values (1);
+INSERT INTO TABLE t0 (c0) VALUES (currentProfiles()); -- { clientError TYPE_MISMATCH }
 
-SELECT * FROM t0 where 1;
-
-INSERT INTO TABLE t0 (c0) VALUES (INTERVAL currentProfiles() MINUTE); -- { clientError ILLEGAL_TYPE_OF_ARGUMENT }
-
-DROP TABLE if exists t0;
+DROP TABLE t0;
